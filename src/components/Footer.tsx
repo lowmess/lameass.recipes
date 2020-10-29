f95acdf47bcba753ed4b16a1d7cb6213de3a4f97
Alec Lomas <alec@lowmess.com>
2020-10-28 18:36:43 -0700

    :lipstick: style footer
    simple for now

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,34 +4,60 @@ import { Box, Flex, Container, Link } from 'theme-ui'
 import { ThemeUIProps } from '../types/ThemeUIComponent'
 
 const Footer: React.FC<ThemeUIProps> = ({ sx, ...props }) => (
-  <Box sx={{ paddingY: 5, backgroundColor: 'muted', ...sx }} {...props}>
+  <Box sx={{ paddingY: 4, backgroundColor: 'muted', ...sx }} {...props}>
     <Container>
       <Flex
         as="ul"
         sx={{
+          flexDirection: ['column', null, 'row'],
           alignItems: 'ceter',
           justifyContent: 'center',
           paddingLeft: 0,
           listStyleType: 'none',
+          fontSize: [2, null, 1],
+          lineHeight: 2,
+          textAlign: 'center',
+
+          'li + li': {
+            marginLeft: [0, null, 4],
+
+            '&::before': {
+              content: [null, null, '"\u2022"'],
+              color: 'accent',
+              marginRight: 4,
+            },
+          },
         }}
       >
-        <Box as="li" mr={4}>
+        <li>
           <NextLink href="/" passHref>
             <Link variant="ui">Home</Link>
           </NextLink>
-        </Box>
+        </li>
 
-        <Box as="li" mr={4}>
+        <li>
           <NextLink href="/recipes" passHref>
             <Link variant="ui">Recipes</Link>
           </NextLink>
-        </Box>
+        </li>
 
-        <Box as="li" mr={4}>
+        <li>
           <NextLink href="/categories" passHref>
             <Link variant="ui">Categories</Link>
           </NextLink>
-        </Box>
+        </li>
+
+        <li>
+          <NextLink href="/tags" passHref>
+            <Link variant="ui">Tags</Link>
+          </NextLink>
+        </li>
+
+        <li>
+          <NextLink href="/about" passHref>
+            <Link variant="ui">About</Link>
+          </NextLink>
+        </li>
       </Flex>
     </Container>
   </Box>

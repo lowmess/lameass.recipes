8ffac8f1d6831faab04e2ad941f7cc81e6056bcd
Alec Lomas <alec@lowmess.com>
2020-10-30 01:23:13 -0700

    :art: use modulo to determine remainder
    duh

--- lib/minutesToHours.ts
+++ lib/minutesToHours.ts
@@ -2,7 +2,7 @@ const minutesToHours = (minutes: number): string => {
   if (minutes < 60) return `${minutes} minutes`
 
   const hours = Math.floor(minutes / 60)
-  const remainder = minutes - hours * 60
+  const remainder = minutes % 60
 
   return `${hours} hours ${remainder} minutes`
 }

6e710a97c1df8d3f667388d4ab60cbebc3bb49ee
Alec Lomas <alec@lowmess.com>
2020-10-29 21:30:47 -0700

    :wheelchair: better accent color contrast
    also i forgot to save my lighthouse config last time

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -19,12 +19,9 @@ module.exports = {
     assert: {
       preset: 'lighthouse:no-pwa',
       assertions: {
-        // I don't have control over these (Gatsby does):
-        // 'uses-rel-preconnect': 'off',
-        // 'unused-javascript': 'off',
-        // 404 page (and only 404 page) causes these to error:
-        // 'tap-targets': 'warn',
-        // 'errors-in-console': 'warn',
+        'unused-javascript': 'off',
+        'tap-targets': 'warn',
+        'errors-in-console': 'warn', // not sure where this is coming from
       },
     },
   },

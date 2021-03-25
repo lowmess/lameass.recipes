acfd6333972dcc17bb54baf38ec7715b46dc09ee
Alec Lomas <alec@lowmess.com>
2021-03-24 21:01:18 -0700

    :bug: correctly fetch swash from meal page

--- src/pages/meals/[slug].tsx
+++ src/pages/meals/[slug].tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import Head from 'next/head'
 import { default as NextLink } from 'next/link'
-import { useThemeUI, Box, Grid, Flex, Text, Heading, Link } from 'theme-ui'
+import { Box, Grid, Flex, Text, Heading, Link } from 'theme-ui'
 import { Tag, Clock, Users } from 'phosphor-react'
 import minutesToHours from '../../../lib/minutesToHours'
 import Stack from '../../components/Stack'
@@ -18,7 +18,6 @@ interface MealPageProps {
 }
 
 const MealPage: React.FC<MealPageProps> = ({ meal }) => {
-	const { colorMode } = useThemeUI()
 	const {
 		title,
 		description,
@@ -45,8 +44,8 @@ const MealPage: React.FC<MealPageProps> = ({ meal }) => {
 					width="512"
 					height="182"
 					alt=""
-					src={`/images/recipe-swash-${colorMode}.png`}
-					srcSet={`/images/recipe-swash-${colorMode}@2x.png 2x`}
+					src={`/images/recipe-swash.png`}
+					srcSet={`/images/recipe-swash@2x.png 2x`}
 					className="swash"
 				/>
 

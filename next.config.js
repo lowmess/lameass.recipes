6e75d2c6f60908753c0b33c6b284de083aadeea2
Alec Lomas <alec@lowmess.com>
2022-05-28 21:53:45 -0700

    :wrench: add vanilla-extract next plugin

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin')
+
+const withVanillaExtract = createVanillaExtractPlugin()
+
 const contentSecurityPolicy = `
 	default-src 'self';
 	script-src 'self' 'unsafe-eval' 'unsafe-inline';
@@ -56,4 +60,4 @@ const nextConfig = {
 	},
 }
 
-module.exports = nextConfig
+module.exports = withVanillaExtract(nextConfig)

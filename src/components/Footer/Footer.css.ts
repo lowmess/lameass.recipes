ef98dde1bd12dcd3c75412e4b0359989e8283dbc
Alec Lomas <alec@lowmess.com>
2022-05-30 16:53:34 -0700

    :sparkles: create footer

---
+++ src/components/Footer/Footer.css.ts
@@ -0,0 +1,106 @@
+import { globalStyle, style } from '@vanilla-extract/css'
+import { vars } from '../../styles/_global.css'
+import { sprinkles } from '../../styles/_sprinkles.css'
+
+export const grid = style([
+	sprinkles({
+		columnGap: ['md', 'lg', null, 'xl'],
+		rowGap: 'lg',
+	}),
+	{
+		display: 'grid',
+		gridTemplateColumns: '1fr',
+
+		'@media': {
+			'screen and (min-width: 26em)': {
+				gridTemplateColumns: '1fr 3fr',
+			},
+
+			'screen and (min-width: 32em)': {
+				gridTemplateColumns: '1fr 2fr',
+			},
+
+			'screen and (min-width: 50em)': {
+				gridTemplateColumns: '1fr 2fr max-content',
+			},
+
+			'screen and (min-width: 64em)': {
+				gridTemplateColumns: '2fr 3fr max-content',
+			},
+		},
+	},
+])
+
+export const sectionHeading = style([
+	sprinkles({
+		marginTop: 'none',
+		marginBottom: 'sm',
+		borderColor: 'gray-1',
+		paddingX: 'sm',
+		fontSize: 'lg',
+		fontWeight: 'medium',
+	}),
+	{
+		borderBottomWidth: '4px',
+		borderBottomStyle: 'solid',
+		lineHeight: 1.25,
+	},
+])
+
+export const list = style([
+	sprinkles({
+		marginY: 'none',
+		paddingX: 'sm',
+	}),
+	{
+		textTransform: 'lowercase',
+		listStyleType: 'none',
+		columnGap: vars.space.md,
+
+		'@media': {
+			'(min-width: 32em)': {
+				columnCount: 2,
+			},
+		},
+	},
+])
+
+globalStyle(`${list} a:hover`, {
+	color: vars.color['gray-1'],
+})
+
+export const graf = sprinkles({
+	marginY: 'none',
+	paddingX: 'sm',
+})
+
+globalStyle(`${graf} a`, {
+	textDecoration: 'underline',
+	textDecorationColor: vars.color['gray-1'],
+})
+
+globalStyle(`${graf} a:hover`, {
+	color: vars.color['gray-1'],
+})
+
+export const tagline = style([
+	sprinkles({
+		marginY: 'none',
+		fontSize: 'xxl',
+		fontWeight: 'semibold',
+	}),
+	{
+		justifySelf: 'center',
+		lineHeight: 1,
+
+		'@media': {
+			'screen and (min-width: 26em)': {
+				gridColumn: '1/3',
+			},
+
+			'(min-width: 50em)': {
+				gridColumn: 'auto',
+			},
+		},
+	},
+])
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react'
+import Link from 'next/link'
+import { Box } from '../Box'
+import { Container } from '../Container'
+import { Text } from '../Text'
+import * as styles from './Footer.css'
+
+export const Footer: React.FC = () => (
+	<Box component="footer" backgroundColor="accent" color="gray-9" paddingY="xl">
+		<Container className={styles.grid}>
+			<div>
+				<h2 className={styles.sectionHeading}>Site</h2>
+
+				<ul className={styles.list}>
+					<li>
+						<Link href="/">Home</Link>
+					</li>
+
+					<li>
+						<Link href="/recipes">Recipes</Link>
+					</li>
+
+					<li>
+						<Link href="/meals">Meals</Link>
+					</li>
+
+					<li>
+						<Link href="/categories">Categories</Link>
+					</li>
+
+					<li>
+						<Link href="/tags">Tags</Link>
+					</li>
+				</ul>
+			</div>
+
+			<div>
+				<h2 className={styles.sectionHeading}>About</h2>
+
+				<p className={styles.graf}>
+					lameass.recipes is a digital cookbook written by{' '}
+					<a href="https://instagram.com/kayla_lameass">Kayla&nbsp;Lomas</a> and
+					designed by <a href="https://lowmess.com">Alec&nbsp;Lomas.</a>
+				</p>
+			</div>
+
+			<h2 className={styles.tagline}>
+				Cooking:
+				<br />
+				&ldquo;It&apos;s <Text color="gray-1">Fun</Text>!&rdquo;&trade;
+			</h2>
+		</Container>
+	</Box>
+)

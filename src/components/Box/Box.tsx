d04f4c9209a1fa741cf9423462ff1e493e643fa6
Alec Lomas <alec@lowmess.com>
2022-05-30 19:31:08 -0700

    :bug: forward ref to box component

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -12,43 +12,11 @@ export interface BoxProps
 	component?: React.ElementType
 }
 
-export const Box: React.FC<BoxProps> = ({
-	component = 'div',
-	className,
-	display,
-	flexDirection,
-	flexWrap,
-	justifyContent,
-	alignItems,
-	gap,
-	rowGap,
-	columnGap,
-	margin,
-	marginX,
-	marginY,
-	marginTop,
-	marginRight,
-	marginBottom,
-	marginLeft,
-	borderColor,
-	padding,
-	paddingX,
-	paddingY,
-	paddingTop,
-	paddingRight,
-	paddingBottom,
-	paddingLeft,
-	fontSize,
-	fontWeight,
-	textAlign,
-	textDecoration,
-	backgroundColor,
-	color,
-	...props
-}) => {
-	const boxClasses = classnames(
-		styles.reset,
-		sprinkles({
+export const Box = React.forwardRef(
+	(
+		{
+			component = 'div',
+			className,
 			display,
 			flexDirection,
 			flexWrap,
@@ -78,9 +46,52 @@ export const Box: React.FC<BoxProps> = ({
 			textDecoration,
 			backgroundColor,
 			color,
-		}),
-		className
-	)
+			...props
+		}: BoxProps,
+		ref
+	) => {
+		const boxClasses = classnames(
+			styles.reset,
+			sprinkles({
+				display,
+				flexDirection,
+				flexWrap,
+				justifyContent,
+				alignItems,
+				gap,
+				rowGap,
+				columnGap,
+				margin,
+				marginX,
+				marginY,
+				marginTop,
+				marginRight,
+				marginBottom,
+				marginLeft,
+				borderColor,
+				padding,
+				paddingX,
+				paddingY,
+				paddingTop,
+				paddingRight,
+				paddingBottom,
+				paddingLeft,
+				fontSize,
+				fontWeight,
+				textAlign,
+				textDecoration,
+				backgroundColor,
+				color,
+			}),
+			className
+		)
 
-	return React.createElement(component, { className: boxClasses, ...props })
-}
+		return React.createElement(component, {
+			ref,
+			className: boxClasses,
+			...props,
+		})
+	}
+)
+
+Box.displayName = 'Box'

460dbedd557044ed278c951d375a91c261c32041
Alec Lomas <alec@lowmess.com>
2022-05-30 23:53:37 -0700

    :zap: don't block rendering with font

--- src/styles/_fonts.css.ts
+++ src/styles/_fonts.css.ts
@@ -3,29 +3,34 @@ import { globalFontFace } from '@vanilla-extract/css'
 globalFontFace('Henrietta', {
 	fontStyle: 'normal',
 	fontWeight: 300,
+	fontDisplay: 'swap',
 	src: `url('/fonts/VCHenrietta-Light.woff2') format('woff2')`,
 })
 
 globalFontFace('Henrietta', {
 	fontStyle: 'normal',
 	fontWeight: 400,
+	fontDisplay: 'swap',
 	src: `url('/fonts/VCHenrietta-Regular.woff2') format('woff2')`,
 })
 
 globalFontFace('Henrietta', {
 	fontStyle: 'normal',
 	fontWeight: 500,
+	fontDisplay: 'swap',
 	src: `url('/fonts/VCHenrietta-Medium.woff2') format('woff2')`,
 })
 
 globalFontFace('Henrietta', {
 	fontStyle: 'normal',
 	fontWeight: 600,
+	fontDisplay: 'swap',
 	src: `url('/fonts/VCHenrietta-SemiBold.woff2') format('woff2')`,
 })
 
 globalFontFace('Henrietta', {
 	fontStyle: 'normal',
 	fontWeight: 700,
+	fontDisplay: 'swap',
 	src: `url('/fonts/VCHenrietta-Bold.woff2') format('woff2')`,
 })

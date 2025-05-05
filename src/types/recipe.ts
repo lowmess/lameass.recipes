1ade2fe0a3277aa0b5c7c7d18c7a03785d1df090
Alec Lomas <alec@lowmess.com>
2025-05-04 19:00:15 -0700

    :label: manually type yields field
    no idea what's going on here

---
+++ src/types/recipe.ts
@@ -0,0 +1,9 @@
+import type { Unit } from "./unit";
+
+// for whatever reason, the `yields` field on recipes are not being typed?
+// i've tried different names in case it's a reserved word conflict...
+// this does not solve the issue.
+export type Measurement = {
+	amount: number;
+	unit: Unit | "servings";
+};
